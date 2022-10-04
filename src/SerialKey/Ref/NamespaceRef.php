a60de12b9d196b179b569e963d6929b4db6b3b03
gs2-ci <ci@gs2.io>
2022-10-04 10:53:24 +0000

    9f83ad23df25a291a79ffbdc7a9ba1ec89ef7f0e

---
+++ src/SerialKey/Ref/NamespaceRef.php
@@ -0,0 +1,79 @@
+<?php /** @noinspection ALL */
+/*
+ * Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+ * Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Gs2Cdk\SerialKey\Ref;
+
+use Gs2Cdk\Core\Func\GetAttr;
+use Gs2Cdk\Core\Func\Join;
+
+class NamespaceRef {
+    public String $namespaceName;
+
+    public function __construct(
+            String $namespaceName,
+    ) {
+        $this->namespaceName = $namespaceName;
+    }
+
+    public function currentCampaignMaster(
+    ): CurrentCampaignMasterRef {
+        return new CurrentCampaignMasterRef(
+            namespaceName: $this->namespaceName,
+        );
+    }
+
+    public function campaignModel(
+            String $campaignModelName,
+    ): CampaignModelRef {
+        return new CampaignModelRef(
+            namespaceName: $this->namespaceName,
+            campaignModelName: $campaignModelName,
+        );
+    }
+
+    public function serialKey(
+            String $serialKeyCode,
+    ): SerialKeyRef {
+        return new SerialKeyRef(
+            namespaceName: $this->namespaceName,
+            serialKeyCode: $serialKeyCode,
+        );
+    }
+
+    public function campaignModelMaster(
+            String $campaignModelName,
+    ): CampaignModelMasterRef {
+        return new CampaignModelMasterRef(
+            namespaceName: $this->namespaceName,
+            campaignModelName: $campaignModelName,
+        );
+    }
+
+    public function grn(): String {
+        return (new Join(
+            ":",
+            [
+                "grn",
+                "gs2",
+                GetAttr::region()->str(),
+                GetAttr::ownerId()->str(),
+                "serialKey",
+                $this->namespaceName
+            ]
+        ))->str();
+    }
+}
---
+++ src/SerialKey/Ref/SerialKeyRef.php
@@ -0,0 +1,62 @@
+<?php /** @noinspection ALL */
+/*
+ * Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+ * Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+namespace Gs2Cdk\SerialKey\Ref;
+
+use Gs2Cdk\Core\Func\GetAttr;
+use Gs2Cdk\Core\Func\Join;
+use Gs2Cdk\SerialKey\StampSheet\UseByUserId;
+
+class SerialKeyRef {
+    public String $namespaceName;
+    public String $serialKeyCode;
+
+    public function __construct(
+            String $namespaceName,
+            String $serialKeyCode,
+    ) {
+        $this->namespaceName = $namespaceName;
+        $this->serialKeyCode = $serialKeyCode;
+    }
+
+    public function use(
+            string $code,
+            string $userId = '#{userId}',
+    ): UseByUserId {
+        return new UseByUserId(
+            namespaceName: $this->namespaceName,
+            userId: $userId,
+            code: $code,
+        );
+    }
+
+    public function grn(): String {
+        return (new Join(
+            ":",
+            [
+                "grn",
+                "gs2",
+                GetAttr::region()->str(),
+                GetAttr::ownerId()->str(),
+                "serialKey",
+                $this->namespaceName,
+                "serialKey",
+                $this->serialKeyCode
+            ]
+        ))->str();
+    }
+}
