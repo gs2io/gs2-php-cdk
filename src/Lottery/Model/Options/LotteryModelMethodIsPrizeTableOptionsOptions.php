a33e844c0175fcb6535478a39835ee7a78054a1e
gs2-ci <ci@gs2.io>
2023-07-10 22:49:53 +0900

    c0885132940eb660c44208034ff251a4e9239082

--- src/Lottery/Model/Options/LotteryModelMethodIsPrizeTableOptionsOptions.php
+++ src/Lottery/Model/Options/LotteryModelMethodIsPrizeTableOptionsOptions.php
@@ -14,24 +14,15 @@
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
+namespace Gs2Cdk\Lottery\Model\Options;
+use Gs2Cdk\Lottery\Model\Enum\LotteryModelMode;
+use Gs2Cdk\Lottery\Model\Enum\LotteryModelMethod;
 
-namespace Gs2Cdk\Formation\Model\Enum;
-
-
-enum SlotWithSignaturePropertyType {
-    case GS2_INVENTORY;
-    case GS2_SIMPLE_INVENTORY;
-    case GS2_DICTIONARY;
-
-    public function toString(): String {
-        switch ($this) {
-            case self::GS2_INVENTORY:
-                return "gs2_inventory";
-            case self::GS2_SIMPLE_INVENTORY:
-                return "gs2_simple_inventory";
-            case self::GS2_DICTIONARY:
-                return "gs2_dictionary";
-        }
-        return "unknown";
-    }
-}
+class LotteryModelMethodIsPrizeTableOptions {
+    public ?string $metadata;
+    
+    public function __construct(
+        ?string $metadata = null,
+    ) {
+        $this->metadata = $metadata;
+    }}
